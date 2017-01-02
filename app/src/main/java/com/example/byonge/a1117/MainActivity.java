5f7fd82d90fd01f9652b2f05752b0b918ebff0ca
PutraNasri <nasrisyawaldi@gmail.com>
2017-01-02 17:22:16 +0700

    2117

--- app/src/main/java/com/example/byonge/a1117/MainActivity.java
+++ app/src/main/java/com/example/byonge/a1117/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.byonge.a1117;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void monitoring(View v){
+        Intent intent = new Intent(MainActivity.this, monitoring.class);
+        startActivity(intent);
+        //finish();
+    }
+    public void control(View v){
+        Intent intent = new Intent(MainActivity.this, control.class);
+        startActivity(intent);
+        //finish();
+    }
+    public void report(View v){
+        Intent intent = new Intent(MainActivity.this, report.class);
+        startActivity(intent);
+        //finish();
     }
 }
